8123819ac73ec140a07d182392081283610c6f2d
harshaal06 <harshalaglawe06@gmail.com>
2024-03-21 23:50:05 +0530

    client add

---
+++ client/src/views/Home/Home.js
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+import './Home.css';
+
+function Home() {
+    const [notes, setNotes] = useState([]);
+
+    const loadNotes = async () =>{
+        const response = await axios.get('http://localhost:5000/notes');
+
+        setNotes(response.data.data);
+    }
+
+    useEffect(()=>{
+        loadNotes();
+    }, []);
+    
+    return (
+    <div>
+        <h1>Home</h1>
+        {
+            notes.map((note, index) => {
+                return(
+                    <div>
+                        <h2>{note.title}</h2>
+                        <p>{note.content}</p>
+                        <span>{note.category}</span>
+                    </div>
+                )
+            })
+        }
+    </div>
+  )
+}
+
+export default Home
