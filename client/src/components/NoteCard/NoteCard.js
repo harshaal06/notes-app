d00ed258f9385c387bfa48264ea48f54d4f53931
harshaal06 <harshalaglawe06@gmail.com>
2024-03-22 15:10:13 +0530

    new note add page

--- client/src/components/NoteCard/NoteCard.js
+++ client/src/components/NoteCard/NoteCard.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import './NoteCard.css'
+import DeleteIcon from "./delete-icon.png";
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
-function NoteCard({_id, title, content, category}) {
+function NoteCard({_id, title, content, category, loadNotes}) {
+
+  const deleteNote = async () => {
+    const response = await axios.delete(`${process.env.REACT_APP_API_URL}/notes/${_id}`)
+    toast.success(response.data.message)
+    loadNotes()
+  }
+
   return (
-    <div class="card my-4 shadow border border-dark-subtle">
+    <div class="card my-4 shadow border border-dark-subtle position-relative">
         <h4 class="card-header border-bottom border-dark-subtle">
             {title}
         </h4>
         <div class="card-body">
             <blockquote class="blockquote mb-0">
             <p>{content}</p>
-            <footer class="blockquote-footer"><cite>{category}</cite></footer>
             </blockquote>
+            <i className='text-bg-secondary px-3 position-absolute category text-center rounded'>{category}</i>
+            <img src={DeleteIcon} 
+              className='bg-primary-subtle position-absolute delete-icon shadow-sm border border-dark-subtle p-2 rounded-circle'
+              alt='delete-icon'
+              onClick={deleteNote}
+              />
         </div>
     </div>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Home from './views/Home/Home';
 import NewNote from './views/NewNote/NewNote';
+import { Toaster } from 'react-hot-toast';
 
 const router = createBrowserRouter([
   {
@@ -19,4 +20,8 @@ const router = createBrowserRouter([
 ])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
+root.render(<>
+  <Toaster />
+  <RouterProvider router={router} />
+  </>
+);
